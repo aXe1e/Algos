ba1847a5ba662b94c4e37fbf31ff3fde7ce98719
aXe1e <axele@list.ru>
2021-04-19 23:09:59 +0500

    Итог ДЗ 1

---
+++ DZ1_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DZ1_2
+{
+    class Program
+    {
+        //2. Посчитайте сложность функции
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;                                            // O(1)
+            for (int i = 0; i < inputArray.Length; i++)             // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++)         // O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)     // O(N)
+                    {
+                        int y = 0;                                  // O(1)
+
+                        if (j != 0)
+                        {
+                            y = k / j;                              // O(1)
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;       // O(1)
+                    }
+                }
+            }
+
+            return sum;
+        }
+        //Сложность функции = O(N) * O(N) * O(N) = O(N^3)
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
