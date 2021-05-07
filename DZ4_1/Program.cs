5cc6758bb4e02edb28d464205891f056ef22fde5
aXe1e <axele@list.ru>
2021-05-07 14:31:00 +0500

    Итог ДЗ 4

--- DZ4_1/Program.cs
+++ DZ4_1/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+
 
 namespace DZ4_1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            BenchmarkRunner.Run<BenchmarkClass>();            
+        }
+        public class BenchmarkClass
+        {
+            private static int MaxElem = 10000;
+            private string[] StrArr;
+            private string Result;
+            private HashSet<string> hashSet;
+
+            public BenchmarkClass()
+            {
+                StrArr = new string[MaxElem];
+                for (int i = 0; i < MaxElem; i++)
+                {
+                    StrArr[i] = Guid.NewGuid().ToString();
+                }
+                hashSet = new HashSet<string>(StrArr);
+                Result = StrArr[MaxElem - 1];   //Результат равен последнему элементу архива.
+            }
+
+            //Тест метода поиска в HashSet
+            [Benchmark]
+            public bool TestHashSetSearch()
+            {                
+                return hashSet.TryGetValue(Result, out var result);
+            }
+
+            //Тест метода поиска в архиве
+            [Benchmark]
+            public bool TestArrSearch()
+            {
+                for(int i = 0; i < StrArr.Length; i++)
+                {
+                    if (StrArr[i].Equals(Result))
+                        return true;
+                }
+                return false;
+            }
         }
     }
 }
---
+++ DZ4_2/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ4_2
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+        public TreeNode(int value)
+        {
+            Value = value;
+        }
+        public override bool Equals(object obj)
+        {
+            var node = obj as TreeNode;
+
+            if (node == null)
+                return false;
+
+            return node.Value == Value;
+        }
+    }
+}
